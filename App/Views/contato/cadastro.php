af1cfe2c0c57b55157295cf1bdbc118004e05769
Leidiane Aguair <leiidiane.aguiar@gmail.com>
2023-06-13 21:40:55 -0300

    Ajustes layout contato

--- App/Views/contato/cadastro.php
+++ App/Views/contato/cadastro.php
@@ -1,33 +1,41 @@
 <link href="http://<?php echo APP_HOST; ?>/public/css/style-dashboard.css" rel="stylesheet">
 
-<div class="container">
-    <h1>Edição de Usuário</h1>
-    <h2>Escreva uma mensagem para o NuAzul</h2>
-    <p>Por favor, preencha os campos para que a mensagem seja enviada</p>
-    <div class="col-md-9">
-        <?php if($Sessao::retornaErro()){ ?>
-            <div class="alert alert-warning" role="alert">
-                <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                <?php foreach($Sessao::retornaErro() as $key => $mensagem) { echo $mensagem . "<br />"; } ?> 
-            </div>
-        <?php } ?>     
+<div class="container" style="margin-left:300px">
+    <div class="row content">
+        <div class="col-md-6 mb-3">
+            <img src="http://<?php echo APP_HOST; ?>/public/img/contato.png" class="img-fluid" alt="image">
+        </div>
+        <div class="col-md-6">
+            <h2 class="signin-text mb-3" style="margin-top:100px;">Escreva uma mensagem para o NuAzul</h2>
+            <p>Por favor, preencha os campos para que a mensagem seja enviada</p>
+            <div >
+                <?php if ($Sessao::retornaErro()) { ?>
+                    <div class="alert alert-warning" role="alert">
+                        <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                        <?php foreach ($Sessao::retornaErro() as $key => $mensagem) {
+                            echo $mensagem . "<br />";
+                        } ?>
+                    </div>
+                <?php } ?>
 
-        <form action="http://<?php echo APP_HOST; ?>/contato/salvar" method="post" id="form_cadastro">
-            <input type="hidden" class="form-control" name="uso_id" id="uso_id" value="<?php echo $viewVar['uso_id']; ?>">
-            <br />
-            <div class="form-group">
-                <label for="titulo">Título</label>
-                <input type="text"  class="form-control" name="titulo" id="titulo" placeholder="" value="" required>
+                <form action="http://<?php echo APP_HOST; ?>/contato/salvar" method="post" id="form_cadastro">
+                    <input type="hidden" class="form-control" name="uso_id" id="uso_id" value="<?php echo $viewVar['uso_id']; ?>">
+                    <br />
+                    <div class="mb-3">
+                        <label for="titulo">Título</label>
+                        <input type="text" class="form-control" name="titulo" id="titulo" placeholder="" value="" required>
+                    </div>
+                    <br />
+                    <div class="mb-3">
+                        <label for="msg">Mensagem</label>
+                        <textarea type="text" class="form-control" name="msg" rows="3" id="msg" placeholder="" value="" required> </textarea>
+                    </div>
+                    <br />
+                    <button type="submit" class="btn btn-primary w100"><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Enviar </button>
+                    <!-- <a href="http://<?php echo APP_HOST; ?>/contato" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Cancelar </a> -->
+                </form>
             </div>
-            <br />
-            <div class="form-group">
-                <label for="msg">Mensagem</label>
-                <textarea type="text"  class="form-control" name="msg" id="msg" placeholder="" value="" required> </textarea>
-            </div>
-            <br />
-            <button type="submit" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span> Enviar </button>          
-            <a href="http://<?php echo APP_HOST; ?>/contato" class="btn btn-info btn-sm"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> Cancelar </a>
-        </form>
+            <div class=" col-md-3"></div>
+        </div>
     </div>
-    <div class=" col-md-3"></div>
 </div>
